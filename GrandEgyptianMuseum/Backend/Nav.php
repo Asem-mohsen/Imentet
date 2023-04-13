fce08781b5b10336c36a71f575531287cb013134
Asem Mohsen <asemmohsen@gmail.com>
2023-04-13 23:35:54 +0200

    new

--- GrandEgyptianMuseum/Backend/Nav.php
+++ GrandEgyptianMuseum/Backend/Nav.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-dark bg-dark">
-                <a class="navbar-brand" href="./Dashboard.php">Imentet</a>
+                <a href="./Dashboard.php"  class="navbar-brand" >
+                    <img src="./Images/LOGO3.png" alt="" width="170px">
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
